01f8fa3516b48078a81e7091c6fcc77fc0a0f809
Jon Reynolds <jon.reynolds456@gmail.com>
2018-06-05 14:31:03 -0500

    initial commit

--- src/redux/editWorkoutReducer.ts
+++ src/redux/editWorkoutReducer.ts
@@ -1,4 +1,4 @@
-import { ActionTypes } from './actions';
+import { ActionKeys, ActionTypes } from './actions';
 import { EditWorkout } from './stateTypes';
 
 const initialState = {
@@ -12,6 +12,21 @@ const editWorkoutReducer = (
   action: ActionTypes
 ): EditWorkout => {
   switch (action.type) {
+    case ActionKeys.EditWorkoutBegin:
+      const { name, type, imageUri } = action.payload;
+      return {
+        name,
+        type,
+        imageUri,
+      };
+    case ActionKeys.EditWorkoutEnd:
+      return initialState;
+    case ActionKeys.EditWorkoutChangeName:
+      return { ...state, name: action.payload };
+    case ActionKeys.EditWorkoutChangeType:
+      return { ...state, type: action.payload };
+    case ActionKeys.EditWorkoutChangeImageUri:
+      return { ...state, imageUri: action.payload };
     default:
       return state;
   }
